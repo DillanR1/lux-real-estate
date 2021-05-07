7cb9e35af743ed2acff3ff7df19ae23bed8b9cb2
DillanR1 <dillanroby@yahoo.com>
2021-05-07 01:28:49 -0700

    fixing landing page

--- public/js/app.js
+++ public/js/app.js
@@ -1,43 +1,62 @@
-console.log("come here often?");
+// console.log("come here often?");
 
-const deleteBtn = document.querySelector(".delete");
+// const deleteBtn = document.querySelector(".delete");
 
-deleteBtn.addEventListener("click", handleDelete);
+// deleteBtn.addEventListener("click", handleDelete);
 
-function handleClick(event) {
-  //GET Request to Properties
-  fetch("/api/v1/properties", {
-    credentials: "include",
-  })
-    .then((stream) => stream.json())
-    .then((data) => renderProperties(data));
+// function handleClick(event) {
+//   //GET Request to Properties
+//   fetch("/api/v1/properties", {
+//     credentials: "include",
+//   })
+//     .then((stream) => stream.json())
+//     .then((data) => renderProperties(data));
+// }
+
+// function renderProperties(propertiesArr) {
+//   const propertiesSection = document.getElementById("properties");
+
+//   propertiesArr.forEach((property) => {
+//     const propertyTemplate = `
+//         <div class="card" style="width:18rem;>
+//             <img style="height: 200px" src="" class=card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">${property.city}</h5>
+//                 <p class="card-text">Description of the property for sale.</p>
+//                 < a href="/properties/${property._id}" class="btn btn-primary float-right">View Details</a>
+//             </div>
+//         </div>
+//       `;
+
+//     propertiesSection.insertAdjacentHTML("beforeend", propertyTemplate);
+//   });
+// }
+
+// function handleDelete(event) {
+//   console.log(event.target.id);
+//   const propertyId = event.target.id;
+//   fetch(`/api/v1/properties/${propertyId}`, {
+//     method: "DELETE",
+//   })
+//     .then((stream) => stream.json())
+//     .then((res) => (window.location.pathname = "/properties"));
+// }
+
+const text1 = document.querySelector("text-1");
+const text2 = document.querySelector("text-2");
+const text3 = document.querySelector("text-3");
+
+const pic1 = document.querySelector("pic-1");
+const pic2 = document.querySelector("pic-2");
+const pic3 = document.querySelector("pic-3");
+
+function reorder(resize) {
+  if (resize.matches) {
+    const 
+
+  }
 }
 
-function renderProperties(propertiesArr) {
-  const propertiesSection = document.getElementById("properties");
 
-  propertiesArr.forEach((property) => {
-    const propertyTemplate = `
-        <div class="card" style="width:18rem;>
-            <img style="height: 200px" src="" class=card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${property.city}</h5>
-                <p class="card-text">Description of the property for sale.</p>
-                < a href="/properties/${property._id}" class="btn btn-primary float-right">View Details</a>
-            </div>
-        </div>
-      `;
 
-    propertiesSection.insertAdjacentHTML("beforeend", propertyTemplate);
-  });
-}
-
-function handleDelete(event) {
-  console.log(event.target.id);
-  const propertyId = event.target.id;
-  fetch(`/api/v1/properties/${propertyId}`, {
-    method: "DELETE",
-  })
-    .then((stream) => stream.json())
-    .then((res) => (window.location.pathname = "/properties"));
-}
+const resize = window.matchMedia("(max-width:900px)");
